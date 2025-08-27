5faef0af3814f26f095da80b718024252937448e
pb4949 <parnilrbalawade@gmail.com>
2025-08-27 14:43:31 +0530

    almost finished
    didn't include merging because too cumbersome and easy, adds alot of code bloat just for a simple function like a list maybe will update later more than 300 lines is not something to write in a book anyways but that might be false sadly doesn't include delete before and delete after number options as well because no time to do in 1 day with lab exams coming up and can't write either

--- 05_linked list.c
+++ 05_linked list.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct node
 {
     int data;
     struct node *nxt;
 };
 
+int linkedListLength(struct node *head)
+{
+    int i = 0; 
+    struct node *ptr = head;
+    while (ptr != NULL)
+    {
+        ptr = ptr->nxt;
+        i++;
+    }
+
+    return i;
+}
 
 void newNodeTail(int val, struct node **head)
 {
@@ -50,31 +61,134 @@ void newNodeHead(int val, struct node **head)
     }
 }
 
-void newNodeChoose(int val, struct node **head, int index, int length)
+void newNodeAt(int val, struct node **head, int index)
 {
-    if (*head == NULL)
-    {
-        (*head) = malloc(sizeof(struct node));
-        (*head)->data = val;
-        (*head)->nxt = NULL;
-    }
+    struct node *ptr;
+    ptr = *head;
+    if (*head == NULL || index <= 0)
+        newNodeHead(val, head);
+    else if (index >= linkedListLength(*head))
+        newNodeTail(val, head);
     else
     {
-        struct node *ptr = *head;
         int i = 0;
-        while (i < index && i < length && ptr != NULL)
+        while (i < index - 1)
         {
             ptr = ptr->nxt;
             i++;
         }
-        if (ptr != NULL)
-            ptr->data = val;
-        else
-            printf("\nINDEX INVALID\n\n");
+        struct node *newPtr;
+        newPtr = malloc(sizeof(struct node));
+        newPtr->data = val;
+        newPtr->nxt = ptr->nxt;
+        ptr->nxt = newPtr;
     }
 }
 
-void displayList(struct node* ptr)
+void newNodeBefore(int val, struct node **head, int num)
+{
+    if (*head == NULL) 
+    {
+        printf("NO NODE INSERTED!\n");
+        return;
+    }
+    if ((*head)->data == num)
+    {
+        newNodeHead(val, head);
+        return;
+    }
+    struct node *ptr = *head;
+    while (ptr->nxt != NULL && ptr->nxt->data != num)
+        ptr = ptr->nxt;
+    if (ptr->nxt == NULL) 
+    {
+        printf("NODE NOT FOUND!\n");
+        return;
+    }
+
+    struct node *newPtr = malloc(sizeof(struct node));
+    newPtr->data = val;
+    newPtr->nxt = ptr->nxt;
+    ptr->nxt = newPtr;
+}
+
+void newNodeAfter(int val, struct node **head, int num)
+{
+    struct node *ptr = *head;
+    while (ptr != NULL && ptr->data != num)
+        ptr = ptr->nxt;
+    if (ptr == NULL) 
+        return;
+
+    struct node *newPtr = malloc(sizeof(struct node));
+    newPtr->data = val;
+    newPtr->nxt = ptr->nxt;
+    ptr->nxt = newPtr;
+}
+
+void delHead(struct node **head)
+{
+    if (*head == NULL) 
+        return;
+    struct node *t = *head;
+    *head = (*head)->nxt;
+    free(t);
+}
+
+void delTail(struct node **head)
+{
+    if (*head == NULL)
+        return;
+    if ((*head)->nxt == NULL)
+    {
+        free(*head);
+        *head = NULL;
+        return;
+    }
+    struct node *ptr = *head;
+    while (ptr->nxt->nxt != NULL)
+        ptr = ptr->nxt;
+    free(ptr->nxt);
+    ptr->nxt = NULL;
+}
+
+void delAt(struct node **head, int index)
+{
+    if (*head == NULL)
+        return;
+    if (index <= 0)
+    {
+        delHead(head);
+        return;
+    }
+    struct node *ptr = *head;
+    int i = 0;
+    while (ptr->nxt != NULL && i < index - 1)
+    {
+        ptr = ptr->nxt;
+        i++;
+    }
+    if (ptr->nxt == NULL) 
+        return;
+    struct node *tmp = ptr->nxt;
+    ptr->nxt = tmp->nxt;
+    free(tmp);
+}
+
+int searchNode(struct node *head, int val)
+{
+    int index = 0;
+    while (head != NULL)
+    {
+        if (head->data == val)
+            return index;
+        head = head->nxt;
+        index++;
+    }
+    return -1; // less than 0 to show no index found
+}
+
+void displayList(struct node* ptr)  
 {
     printf("\nList-\n");
     while (ptr != NULL)
@@ -83,19 +197,6 @@ void displayList(struct node* ptr)
         ptr = ptr->nxt;
     }
     printf("\n\n");
-}
-
-int linkedListLength(struct node *head)
-{
-    int i = 0; 
-    struct node *ptr = head;
-    while (ptr != NULL)
-    {
-        ptr = ptr->nxt;
-        i++;
-    }
-
-    return i;
 }
 
 int main()
@@ -110,6 +211,8 @@ int main()
     printf("0. Exit\n");
     printf("1. New Node\n");
     printf("2. View List\n");
+    printf("3. Delete Node\n");
+    printf("4. Search\n");
     scanf("%d", &ch);
     switch(ch)
     {
@@ -126,6 +229,15 @@ int main()
                 printf("\nNO LIST CREATED!\n\n");
             goto mainMenu;
             break;
+        case 3: 
+            goto delMenu;
+            break;
+        case 4:
+            printf("Number: ");
+            scanf("%d", &index); // reused, actually the number you want to search
+            printf("Number found at: [%d] (-1 if not found)\n", searchNode(head, index));
+            goto mainMenu;
+            break;
         default:
             printf("\nINVALID INPUT, TRY AGAIN\n\n");
             goto mainMenu;
@@ -135,9 +247,11 @@ int main()
     nodeMenu:
     printf("<------------Node Actions------------>\n");
     printf("0. Back\n");
-    printf("1. Insert at End\n");
-    printf("2. Insert at Beginning\n");
-    printf("3. Insert in Between\n");
+    printf("1. Insert at Tail\n");
+    printf("2. Insert at Head\n");
+    printf("3. Insert at Index\n");
+    printf("4. Insert before Number\n");
+    printf("5. Insert after Number\n");
     scanf("%d", &ch);
 
     switch (ch)
@@ -160,11 +274,54 @@ int main()
             scanf("%d", &val);
             printf("Index: ");
             scanf("%d", &index);
-            newNodeChoose(val, &head, index, linkedListLength(head));
-            break; 
+            newNodeAt(val, &head, index);
+            break;
+        case 4:
+            printf("Value: ");
+            scanf("%d", &val);
+            printf("Number: ");
+            scanf("%d", &index); // repurposed var
+            newNodeBefore(val, &head, index);
+            break;
+        case 5:
+            printf("Value: ");
+            scanf("%d", &val);
+            printf("Number: ");
+            scanf("%d", &index); // repurposed var
+            newNodeAfter(val, &head, index);
+            break;
+        
         default: 
             printf("\nINVALID INPUT, TRY AGAIN\n\n");
             break;
     }
     goto nodeMenu;
+
+    delMenu:
+    printf("<-----------Deletion Menu------------>\n");
+    printf("0. Back\n");
+    printf("1. Delete at Head\n");
+    printf("2. Delete at Tail\n");
+    printf("3. Delete at Index\n");
+    scanf("%d", &ch);
+    switch(ch)
+    {
+        case 0:
+            goto mainMenu;
+        case 1:
+            delHead(&head);
+            break;
+        case 2:
+            delTail(&head);
+            break;
+        case 3:
+            printf("Index: ");
+            scanf("%d", &index);
+            delAt(&head, index);
+            break;
+        default:
+            printf("\nINVALID INPUT, TRY AGAIN\n");
+            break;
+    }
+    goto delMenu;
 }
