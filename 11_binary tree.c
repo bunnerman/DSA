6553a8e5ec32e8e5b60b1d0f183b3f4e8a1df2bb
bunnerman <parnilrbalawade@gmail.com>
2025-10-21 23:24:57 +0530

    Update 11_binary tree.c

--- 11_binary tree.c
+++ 11_binary tree.c
@@ -5,30 +5,63 @@
 struct node
 {
 	int data;
-	struct node *left;
-	struct node *right;
+	struct node* left;
+	struct node* right;
+	struct node* back;
 };
 
-struct node *root = NULL;
+struct node* root = NULL;
 
 void displayNodeData(struct node *currentNode)
 {
-	printf("Data: %d\n", currentNode->data);
+	if (currentNode->left == NULL)
+		printf("\nNULL <--");
+	else
+		printf("\n%d <--", currentNode->left->data);
+	printf(" %d ", currentNode->data);
+	if (currentNode->right == NULL)
+		printf("--> NULL\n");
+	else
+		printf("--> %d\n", currentNode->right->data);
+	if (currentNode->back != NULL)
+		printf("Parent: %d\n\n", currentNode->back->data);
+	else	
+		printf("Parent: NULL (root node)\n\n");
+}
+
+void inorderTraversal()
+{
+	struct node* currentNode = root;
+	int stack[100]; // Large Enough
+	while (1)
+	{
 		if (currentNode->left == NULL)
-			printf("<-- NULL\n");
+		{
+			if (currentNode->right == NULL)
+				currentNode = currentNode->right;
+			else
+				currentNode = currentNode->back;
+		}
 		else
-			printf("<-- %d\n", (currentNode->left)->data);
-		if (currentNode->right == NULL)
-			printf("--> NULL\n");
-		else
-			printf("--> %d\n", (currentNode->right)->data);
+			currentNode = currentNode->left;
+	}
 }
 
 void freeNav()
 {
 	char ch;
 	int temp;
-	struct node *currentNode = root;
+	struct node* currentNode = root;
+	if (root == NULL)
+	{
+		root = malloc(sizeof(struct node));
+		printf("[INTLZTN] Enter value of root node: ");
+		scanf("%d", &(root->data));
+		root->left = NULL;
+		root->right = NULL;
+		root->back = NULL;
+		currentNode = root;
+	}
 	do 
 	{
 		displayNodeData(currentNode);
@@ -36,22 +69,35 @@ void freeNav()
 		scanf(" %c", &ch);
 		if (ch == 'D') // Deletion
 		{
-			if (currentNode->left == NULL || currentNode->right != NULL)
+			if (currentNode->left != NULL || currentNode->right != NULL)
 			{
 				printf("Node is a parent, Deletion Failed\n");
 				continue;
 			}
-			currentNode = NULL;
-			printf("Node Deleted\n");
+			if (currentNode == root)
+			{
+				root = NULL;
+				printf("Root Node Deleted, Ending Navigation Mode\n");
+				return;
+			}
+			struct node* parentNode = currentNode->back;
+			free(currentNode);
+			printf("Node Deleted, rerouting to parent node\n");
+			currentNode = parentNode;
+			continue;
+		}
+		else if (ch == 'B')
+		{
+			currentNode = currentNode->back;
 			continue;
 		}
 		else if (ch == 'S')
 			return;
 			
-		struct node *prvNode = currentNode;
+		struct node* prvNode = currentNode;
 		if (ch == 'L')
 			currentNode = currentNode->left;
-		else
+		else if (ch == 'R')
 			currentNode = currentNode->right;
 
 		if (currentNode == NULL)
@@ -66,6 +112,7 @@ void freeNav()
 			currentNode->data = temp;
 			currentNode->left = NULL;
 			currentNode->right = NULL;
+			currentNode->back = prvNode;
 		}
 	} 
 	while (ch != 'S');
@@ -82,16 +129,11 @@ int main()
 	printf("\tR - Right\n");
 	printf("\tS - Stop Free Navigation\n");
 	printf("\tD - Delete Node\n");
+	printf("\tB - Previous Node\n");
 	printf("2. Full Display\n");
 	printf("3. In-Order Display\n");
 	printf("4. Pre-Order Display\n");
 	printf("5. Post-Order Display\n");
-	
-	root = malloc(sizeof(struct node));
-	printf("[INTLZTN] Enter value of root node: ");
-	scanf("%d", &(root->data));
-	root->left = NULL;
-	root->right = NULL;
 
 	do 
 	{
@@ -122,5 +164,6 @@ int main()
 		}
 	} 
 	while (ch != 0);
+
 	return 0;
 }
